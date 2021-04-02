6d455050ee05d59ee30b974af7d04f31a2fbfe0f
Manuel <ercm.mvrc@gmail.com>
2021-04-02 15:12:12 +0200

    mosaic 10

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Intermediate from './Intermediate1';
+import Intermediate from './Intermediate';
 
 ReactDOM.render(
 	<Intermediate />, 
